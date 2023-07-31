88f2b41ce923232a9906409d68acaa1da1f450d4
Kevin He <kevin.he0923@gmail.com>
2023-07-31 08:35:01 -0700

    fix: add variants to Button

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Pressable, Text, StyleSheet } from 'react-native';
 
 type ButtonProps = {
   label: JSX.Element | string;
+  variant?: 'primary' | 'secondary';
   onPress: () => void;
 };
 
-export const Button: React.FC<ButtonProps> = ({ label, onPress }) => (
-  <Pressable style={styles.boxShadow} onPress={onPress}>
-    <Text>{label}</Text>
-  </Pressable>
-);
+export const Button: React.FC<ButtonProps> = ({ label, variant, onPress }) => {
+  const btnStyle = useMemo(() => {
+    const style: any[] = [styles.button, styles.boxShadow];
+
+    switch (variant) {
+      case 'primary': {
+        style.push(styles.primaryBtn);
+        break;
+      }
+      default:
+        break;
+    }
+
+    return style;
+  }, [variant]);
+
+  return (
+    <Pressable style={btnStyle} onPress={onPress}>
+      <Text style={styles.btnText}>{label}</Text>
+    </Pressable>
+  );
+};
 
 const styles = StyleSheet.create({
+  button: {
+    padding: 12,
+  },
   boxShadow: {
     elevation: 4,
     shadowColor: '#000',
@@ -24,6 +45,15 @@ const styles = StyleSheet.create({
     shadowRadius: 2,
   },
   container: {},
+  primaryBtn: {
+    backgroundColor: '#002B56',
+    borderRadius: 8,
+  },
+  btnText: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: 'white',
+  },
 });
 
 export default Button;
